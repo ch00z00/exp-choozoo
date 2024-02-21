ec39a6da8986fd1b0e429513683dcfecfd4c025a
ch00z00 <choozoo10@gmail.com>
2024-02-21 11:45:40 +0900

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,35 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    extend: {},
+    colors: {
+      white: {
+        100: "#FFFFFF",
+        200: "#F5F5F5",
+        300: "#D8D8D8",
+      },
+      black: {
+        100: "#000000",
+        200: "#19191A",
+        300: "#272b29",
+        400: "#212623",
+      },
+      red: {
+        100: "#F40009",
+      },
+      blue: {
+        100: "#EDF2F6",
+        200: "#1F3250",
+      },
+      green: {
+        100: "#415245",
+        200: "#747C16",
+      },
+      orange: {
+        100: "#EF3E1D",
+      },
+      yellow: {
+        100: "#CDC8BC",
       },
     },
   },
